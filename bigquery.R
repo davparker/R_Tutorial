0aaca74c83a0c2846ebbece3e85774945df0a39b
David Parker <davparker@gmail.com>
2014-10-07 21:49:36 -0500

    rmongodb.Rmd finished Intro rmongodb vignette

--- bigquery.R
+++ bigquery.R
@@ -1,12 +1,12 @@
 # http://thinktostart.wordpress.com/2014/03/10/using-google-bigquery-with-r/
 # Using Google BigQuery with R
 # BY JULIANHI MARCH 10, 2014 BIGQUERY DATA GOOGLE QUERY R SQL
+library("devtools")
+devtools::install_github("hadley/assertthat")
+devtools::install_github("hadley/bigrquery")
 
-devtools::install_github("assertthat")
-devtools::install_github("bigrquery")
-
+#  https://cloud.google.com/console
 library(bigrquery)
-project <- "decisive-circle-666" # put your projectID here
-sql <- 'SELECT title,contributor_username,comment FROM[publicdata:samples.wikipedia] WHERE title CONTAINS "beer" LIMIT 100;'
-mydata <- query_exec("publicdata", "samples", sql, billing = project)
-mydata
+project <- "warm-classifier-725" # put your project ID here
+sql <- "SELECT year, month, day, weight_pounds FROM [publicdata:samples.natality] LIMIT 5"
+query_exec(sql, project = project)
